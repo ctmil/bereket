f624ef3df7e55a7f0e2a9870b755f601ad5e5855
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-05-31 17:36:50 +0200

    Upgrade shipping_list_cost and receiver

--- meli_oerp/models/shipment.py
+++ meli_oerp/models/shipment.py
@@ -390,7 +390,7 @@ class mercadolibre_shipment(models.Model):
                 _logger.info('MEL Distribution, not adding to order')
                 #continue
 
-            del_price = shipment.shipping_cost
+            del_price = shipment.shipping_cost or shipment.shipping_list_cost
             delivery_price = ml_product_price_conversion( self, product_related_obj=product_shipping_id, price=del_price, config=config ),
             if type(delivery_price)==tuple and len(delivery_price):
                 delivery_price = delivery_price[0]
