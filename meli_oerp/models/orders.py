91e7443ddfd3d13a29c35b62ade986ae348db981
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-08-15 19:44:14 +0200

    Upgrading

--- meli_oerp/models/orders.py
+++ meli_oerp/models/orders.py
@@ -1956,13 +1956,13 @@ class mercadolibre_orders(models.Model):
                             offset_next = offset + orders_json["paging"]["limit"]
                         _logger.info("offset_next:"+str(offset_next))
 
-        _logger.info( orders_json )
+        #_logger.info( orders_json )
         if "results" in orders_json:
             for order_json in orders_json["results"]:
                 if order_json:                    
                     pdata = {"id": False, "order_json": order_json}
                     if "id" in order_json and fetch_id_only:
-                        _logger.info( order_json["id"] )
+                        #_logger.info( order_json["id"] )
                         __fetch_ids.append(str(order_json["id"]))
                     else:
                         try:
